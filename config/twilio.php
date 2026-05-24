f666d7c6a182225f85a02cfdc84a3030ffc6fe08
Adriano D Machado <adriano@systax.com.br>
2026-05-24 18:48:54 -0300

    config twilio

---
+++ config/twilio.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'account_sid' => env('TWILIO_ACCOUNT_SID'),
+    'auth_token' => env('TWILIO_AUTH_TOKEN'),
+    'phone_number' => env('TWILIO_PHONE_NUMBER'),
+    'new_message_url' => env('TWILIO_NEW_MESSAGE_URL'),
+    'from' => env('WHATS_APP_FROM'),
+];
